6ad4e87cc3ac21d066e48d8aab189879c6ca5638
atm-gregr <greg.rastklan@atm-consulting.fr>
2021-05-24 12:26:19 +0200

    fix

--- class/actions_dispatch.class.php
+++ class/actions_dispatch.class.php
@@ -267,6 +267,11 @@ class ActionsDispatch
 						foreach ($TRecepDetail as $detail) {
 							// Grouping with conf
 //							if (!empty($conf->global->DISPATCH_GROUP_DETAILS_ON_PDF)) {
+								if ($detail->fk_asset != 0)
+								{
+									$asset = new TAsset;
+									$asset->load($PDOdb, $detail->fk_asset);
+								}
 								$newComparaison = $this->getArrayForAssetToLineDescCompare($detail, $asset, $outputlangs);
 
 								$isGrouped = false;
